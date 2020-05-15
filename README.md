ed78a2ed4a4982725a26ad8c8ae079cd9252f3cf
Russell Smith <Russell.G.Smith@jpl.nasa.gov>
2020-05-15 00:52:42 -0700

    Expanded argparse options and Readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-run ./automaton_animated rule size
+Note: can edit kernel and grid manually to change evolution and initial conditions (eg probability distribution of (1,0) in grid or initial seed)
 
-can edit kernel and grid to change evolution and initial conditions (eg probability distribution of (1,0) in grid or initial seed)
+
+-rs should be a val between 0 and 1... argparse got a little gnarly
+
+
+
+
+usage: automaton_animated.py [-h] [-r RULE] [-i ITERATIONS] [-f FRAMERATE]
+                             [-d]
+                             [-rs {<__main__.Range object at 0x7f70a0557ac8>}]
+
+Generate Automatons
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -r RULE, --rule RULE  Choose a rule 0-255
+  -i ITERATIONS, --iterations ITERATIONS
+                        Number of iterations to run
+  -f FRAMERATE, --framerate FRAMERATE
+                        attempt to run at given framerate FPS
+  -d, --demo            Run demo mode with preset pretty thing
+  -rs {<__main__.Range object at 0x7f70a0557ac8>}, --random_seed {<__main__.Range object at 0x7f70a0557ac8>}
+                        Init with random seed: percent fill with 1s (very
+                        big/small most interesting)
+
--- automaton_animated.py
+++ automaton_animated.py
@@ -9,31 +9,53 @@ import math
 import cv2
 import imutils
 
+class Range(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def __eq__(self, other):
+        return self.start <= other <= self.end
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Automatons')
-    parser.add_argument('rule', help='Choose a rule 0-255')
-    parser.add_argument('iterations', help='Number of iterations to run')
+    parser.add_argument('-r', '--rule', type=int, default=42, help='Choose a rule 0-255')
+    parser.add_argument('-i', '--iterations', type=int, default=500, help='Number of iterations to run')
+    parser.add_argument('-f', '--framerate', type=float, default=10, help='attempt to run at given framerate FPS')
+    parser.add_argument('-d', '--demo', help='Run demo mode with preset pretty thing', action='store_true')
+    parser.add_argument('-rs', '--random_seed', type=float, choices=[Range(0.0,1.0)], default=None, help='Init with random seed: percent fill with 1s (very big/small most interesting)')
     args = parser.parse_args()
 
-    rule = int(args.rule)
-    binary_rule = np.flip([rule >> i & 1 for i in range(7,-1,-1)])
-    # binary_rule = np.logical_not(binary_rule).astype(int)
-    iterations = int(args.iterations)
     init = 1
     pad = 1
-    center = math.floor((iterations*2+pad)/2)
-    grid = np.zeros((iterations, iterations*2+pad))
-    #grid = np.random.choice(2,2*iterations**2, p=[0.005,0.995]).reshape(iterations,2*iterations).astype(float)
 
-    grid[math.floor(iterations/2)][center] = init
-    print("")
-    print(grid)
+    if args.demo:
+        rule = 42
+        iterations = 500
+        kernel = np.array((
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]), dtype="int")
+        grid = np.zeros((iterations, iterations*2+pad))
+        framerate = 100
+    else:                                                       # CHANGE THESE VALUES
+        rule = int(args.rule)
+        iterations = int(args.iterations)
+        kernel = np.array((
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]), dtype="int")
+        framerate = int(args.framerate)
+        if args.random_seed:
+            grid = np.random.choice(2,2*iterations**2, p=[1-args.random_seed,args.random_seed]).reshape(iterations,2*iterations).astype(float)
+        else:
+            grid = np.zeros((iterations, iterations*2+pad))
+            center = math.floor((iterations*2+pad)/2)
+            grid[math.floor(iterations/2)][center] = init
 
-    kernel = np.array((
-        [-1, 1, -1],
-        [1, 1, 1],
-        [-1, 1, -1]), dtype="int")
-    
+    binary_rule = np.flip([rule >> i & 1 for i in range(7,-1,-1)])
+    # binary_rule = np.logical_not(binary_rule).astype(int)
+
+
     s = time.time()
 
     while True:
@@ -42,7 +64,7 @@ if __name__ == '__main__':
         #grid_show = cv2.resize(grid, width=540, interpolation = cv2.INTER_NEAREST) 
         grid_show = imutils.resize(grid,width=1000)
         cv2.imshow("img",grid_show)
-        cv2.waitKey(100)
+        cv2.waitKey(int(1/framerate * 1000)) #ms
         e = time.time()
         print('Framerate: %i FPS\r'%(1.0/(e-s)), end="")
         s = time.time()
